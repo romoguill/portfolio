10c8ac93ea5e1a47bcf3b1e6be9e10093044844f
Rodrigo Moguillansky <romoguill@gmail.com>
2024-07-21 20:45:14 -0300

    Fixed minor styles in cards

--- src/components/Projects/ProjectsCard.tsx
+++ src/components/Projects/ProjectsCard.tsx
@@ -1,3 +1,4 @@
+import { indieFlower, inter, robotoMono, rubik } from '@/src/app/fonts';
 import Image from 'next/image';
 import { FaGithub } from 'react-icons/fa6';
 
@@ -20,11 +21,13 @@ function ProjectsCard({
     <article className='rounded-xl overflow-hidden w-80 bg-secondary-900 relative shadow-md'>
       <div className='p-3'>
         <div className='flex justify-between items-center mb-2'>
-          <h3 className='text-lg'>{title}</h3>
+          <h3 className={`text-2xl tracking-wide ${robotoMono.className}`}>
+            {title}
+          </h3>
           <div className='flex gap-1 items-stretch justify-between'>
             <a
               href={githubLink}
-              className='rounded-tl-md rounded-bl-md bg-black p-2 hover:bg-neutral-900'
+              className='rounded-tl-md rounded-bl-md bg-black p-2 hover:bg-accent-secondary/60 transition'
               target='_blank'
             >
               <FaGithub size={22} />
@@ -32,7 +35,7 @@ function ProjectsCard({
 
             <a
               href={deployedLink}
-              className='rounded-tr-md rounded-br-md bg-accent-secondary p-2 hover:bg-accent-secondary/80'
+              className='rounded-tr-md rounded-br-md bg-[#234845] p-2 hover:bg-accent-secondary/60 transition'
               target='_blank'
             >
               Live
