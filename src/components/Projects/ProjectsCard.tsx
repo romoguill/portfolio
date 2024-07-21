0e6981b8d5e5180d41d03f4b784f11f71b61ad7d
Rodrigo Moguillansky <romoguill@gmail.com>
2024-07-21 20:33:16 -0300

    Added github and live links for projects cards

--- src/components/Projects/ProjectsCard.tsx
+++ src/components/Projects/ProjectsCard.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { FaGithub } from 'react-icons/fa6';
 
 interface ProjectsCardProps {
   title: string;
@@ -16,13 +17,26 @@ function ProjectsCard({
   deployedLink,
 }: ProjectsCardProps) {
   return (
-    <article className='rounded-xl overflow-hidden w-80 bg-sky-950 relative shadow-md'>
+    <article className='rounded-xl overflow-hidden w-80 bg-secondary-900 relative shadow-md'>
       <div className='p-3'>
         <div className='flex justify-between items-center mb-2'>
           <h3 className='text-lg'>{title}</h3>
-          <div>
-            <a href={githubLink}>Github</a>
-            <a href={deployedLink}>Live</a>
+          <div className='flex gap-1 items-stretch justify-between'>
+            <a
+              href={githubLink}
+              className='rounded-tl-md rounded-bl-md bg-black p-2 hover:bg-neutral-900'
+              target='_blank'
+            >
+              <FaGithub size={22} />
+            </a>
+
+            <a
+              href={deployedLink}
+              className='rounded-tr-md rounded-br-md bg-accent-secondary p-2 hover:bg-accent-secondary/80'
+              target='_blank'
+            >
+              Live
+            </a>
           </div>
         </div>
         <p>{description}</p>
