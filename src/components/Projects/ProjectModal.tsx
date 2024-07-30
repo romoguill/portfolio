4523730827614cd187035e72360e2795f50c7fec
Rodrigo Moguillansky <romoguill@gmail.com>
2024-07-30 20:05:05 -0300

    Adjusted styles in modal and added placeholder for video

--- src/components/Projects/ProjectModal.tsx
+++ src/components/Projects/ProjectModal.tsx
@@ -3,6 +3,7 @@
 import { Dictionary } from '@/src/app/[lang]/dictionaries';
 import * as Dialog from '@radix-ui/react-dialog';
 import { SetStateAction, useState } from 'react';
+import ProjectVideo from './ProjectVideo';
 
 interface ProjectModalProps {
   isOpen: boolean;
@@ -15,9 +16,12 @@ function ProjectModal({ isOpen, setIsOpen }: ProjectModalProps) {
     <Dialog.Root open={isOpen} onOpenChange={setIsOpen}>
       <Dialog.Portal>
         <Dialog.Overlay className='fixed inset-0 backdrop-blur-sm bg-black/60 z-50' />
-        <Dialog.Content className='fixed top-1/2 left-1/2 -translate-y-[50%] -translate-x-[50%] w-80 h-60 bg-white z-50'>
+        <Dialog.Content className='fixed top-1/2 left-1/2 -translate-y-[50%] -translate-x-[50%] w-1/3 h-[700px] bg-primary-950 z-50 p-6'>
           <Dialog.Title>Project name</Dialog.Title>
           <Dialog.Description>some desc</Dialog.Description>
+          <div className='flex flex-col'>
+            <ProjectVideo />
+          </div>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
---
+++ src/components/Projects/ProjectVideo.tsx
@@ -0,0 +1,7 @@
+function ProjectVideo() {
+  return (
+    <div className='bg-neutral-600 border-white w-full h-52'>ProjectVideo</div>
+  );
+}
+
+export default ProjectVideo;
