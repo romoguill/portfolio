52a8cabf8920f3eab0bf1d1496943e9a8b9d8b63
romoguill <romoguill@gmail.com>
2024-11-07 20:37:07 -0300

    Next build error due to fonts

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -9,20 +9,24 @@ import {
 export const rubik = Rubik({
   subsets: ['latin'],
   variable: '--font-rubik',
+  display: 'swap',
 });
 
 export const inter = Inter({
   subsets: ['latin'],
   variable: '--font-inter',
+  display: 'swap',
 });
 
 export const robotoMono = Roboto_Mono({
   subsets: ['latin'],
   variable: '--font-roboto-mono',
+  display: 'swap',
 });
 
 export const indieFlower = Indie_Flower({
   subsets: ['latin'],
   variable: '--font-indie-flower',
   weight: '400',
+  display: 'swap',
 });
