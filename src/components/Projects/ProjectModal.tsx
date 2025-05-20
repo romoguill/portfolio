aef55a378d2582212d9aabca1d2fd4b1f613364e
romoguill <romoguill@gmail.com>
2025-05-20 11:07:07 -0300

    Added jigsaw project content in spanish

--- src/components/Projects/ProjectModal.tsx
+++ src/components/Projects/ProjectModal.tsx
@@ -22,13 +22,17 @@ function ProjectModal({ isOpen, setIsOpen, data }: ProjectModalProps) {
         <Dialog.Overlay className='fixed inset-0 backdrop-blur-sm bg-black/60 z-40' />
         <Dialog.Content className='fixed top-1/2 left-1/2 -translate-y-[50%] -translate-x-[50%]  w-full max-w-lg md:max-w-2xl bg-primary-950 z-50 p-3 text-neutral-100'>
           <button
-            className='absolute top-3 right-3 text-neutral-100 text-2xl cursor-pointer z-50'
+            className='absolute top-3 right-8 text-neutral-100 text-2xl cursor-pointer z-50'
             onClick={() => setIsOpen(false)}
           >
-            <IoCloseCircle size={40} className='fill-red-500' />
+            <IoCloseCircle
+              size={40}
+              className='fill-red-500 focus:outline-none'
+            />
           </button>
 
-          <ScrollArea.Root className='h-[700px] overflow-hidden'>
+          <ScrollArea.Root className='h-[700px] overflow-hidden relative'>
+            <div className='absolute top-0 left-0 right-0 h-8 bg-gradient-to-b from-primary-950 to-transparent z-10 pointer-events-none' />
             <ScrollArea.Viewport className='h-full w-full p-5'>
               <Dialog.Title className='text-2xl tracking-wider'>
                 {data.name}
@@ -37,22 +41,39 @@ function ProjectModal({ isOpen, setIsOpen, data }: ProjectModalProps) {
               <div className='flex flex-col gap-5'>
                 <ProjectDisplay type='image' url={data.imageUrl} />
 
-                <article>
-                  <p>{data.description}</p>
-                  <h3 className={`text-lg uppercase ${robotoMono.className}`}>
-                    Features
-                  </h3>
-                  <p></p>
-                </article>
-                <article>
-                  <h3 className={`text-lg uppercase ${robotoMono.className}`}>
-                    Tech Stack
-                  </h3>
-                  <p></p>
+                <article className='space-y-3'>
+                  <div>
+                    <h3
+                      className={`${robotoMono.className} text-yellow-300 mb-1 tracking-wider`}
+                    >
+                      {data.sections[0]}
+                    </h3>
+                    <p className='whitespace-pre-line'>{data.description}</p>
+                  </div>
+                  <div>
+                    <h3
+                      className={`${robotoMono.className} text-yellow-300 mb-1 tracking-wider`}
+                    >
+                      {data.sections[1]}
+                    </h3>
+                    <ul className='list-disc  list-inside'>
+                      {data.features.map((feature) => (
+                        <li key={feature}>{feature}</li>
+                      ))}
+                    </ul>
+                  </div>
+                  <div>
+                    <h3
+                      className={`${robotoMono.className} text-yellow-300 mb-1 tracking-wider`}
+                    >
+                      {data.sections[2]}
+                    </h3>
+                    <p>{data.techStack}</p>
+                  </div>
                 </article>
               </div>
             </ScrollArea.Viewport>
-
+            <div className='absolute bottom-0 left-0 right-0 h-8 bg-gradient-to-t from-primary-950 to-transparent z-10 pointer-events-none' />
             <ScrollArea.Scrollbar
               className='flex select-none touch-none p-0.5 bg-slate-950 transition-colors duration-[160ms] ease-out hover:bg-black data-[orientation=vertical]:w-2.5 data-[orientation=horizontal]:flex-col data-[orientation=horizontal]:h-2.5'
               orientation='vertical'
